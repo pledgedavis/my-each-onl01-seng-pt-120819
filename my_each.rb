336149925f33d98618792e3d8380e276a98fad44
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 17:25:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s)here
   # code here
+  i=0
+  collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
+  i = i + 1
+end
+collection
 end
